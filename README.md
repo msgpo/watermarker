0a8f8a6dbb8eca2a9a2bd9775d2972236e0d0770
yongzhihuang <yongzhihuang@yahoo.com>
2018-03-07 18:33:06 -0500

    Use another lib

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ##Usage:
 `npm start` then head over to 
-http://localhost:1337/?image=https://www.tradingview.com/x/sknltJb0/&text=Free chart analysis only on http://cryptodam.us/chat
-
+http://localhost:1337/?image=https://www.tradingview.com/x/sknltJb0/&text=Follow Me for chart analysis. Or join my discord at http://cryptodam.us/chat for alerts and real time charts!
 ###Parameters
 - image - path of image, from external url
 - text - text to appear on the image
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const watermark = require('image-watermark');
 
+const caption = require('caption');
+
 const fs = require('fs'),
 request = require('request');
 
@@ -19,23 +21,24 @@ const app = express();
 
 app.get('/', (req, res) => {
     const imagePath = req.query.image;
-    const overlayText = req.query.text;
+    // const defaultOverlayText = 'Follow me for chart analysis or join my discord at http://cryptodam.us';
+    const defaultOverlayText = 'Follow Me on http://cryptodam.us';
+    const overlayText = req.query.text || defaultOverlayText;
+
     if (!imagePath || !overlayText) {
       res.send('Error, please provide correct parameters');
     }
 
     fetchImage(imagePath, 'source.png', () => {
-        const options = {
-          text : overlayText,
-          font: 'arial',
-          align: 'ltr',
-          color: 'rgb(255, 255, 255)'
-        };
-        watermark.embedWatermarkWithCb('./source.png', options, (err) => {
-          if (!err) {
-            res.download('./watermark.png', 'output.png');
-          }
-        });
+        caption.path('./source.png',{
+          caption : overlayText,
+          bottomCaption: 'Charts, Alerts, Community',
+          outputFile : 'watermark.png',
+          minHeight: 300,
+          minWidth: 700
+        },function(err,filename){
+          res.download(`./watermark.png`, 'output.png');
+        })
 
     });
 
